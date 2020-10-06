4264712fce9af91b7681629f2a031c65e30382fc
unknown <dlorenzo.05@gmail.com>
2020-10-06 17:22:12 +0200

    button.jsx component medified

--- client/app/components/button/button.jsx
+++ client/app/components/button/button.jsx
@@ -1,10 +1,32 @@
-import React from "react";
-import styles from "./button.scss";
+import React from 'react'
+import classnames from 'classnames/bind'
 
-const Button = () => (
-  <button className={styles.button} type="button">
-    Click me
-  </button>
-);
+import styles from './button.scss'
 
-export default Button;
+const cx = classnames.bind(styles)
+
+const Button = (props) => {
+  const handleButtonClick = ({ value, type }) => {
+    onClick({ value, type })
+  }
+
+  const { value, theme, color, size, type, text, onClick } = props
+
+  return (
+    <div>
+      <button
+        className={cx('button', {
+          [`button--theme-${theme}`]: theme,
+          [`button--${size}`]: size,
+          [`button--${color}`]: color,
+        })}
+        onClick={() => {
+          handleButtonClick({ value, type })
+        }}>
+        {text}
+      </button>
+    </div>
+  )
+}
+
+export default Button
