8f3eb9500c16daeb124541985ba5b5a503dc6446
unknown <dlorenzo.05@gmail.com>
2020-10-06 16:52:15 +0200

    added client/ folder

---
+++ client/app/components/button/button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./button.scss";
+
+const Button = () => (
+  <button className={styles.button} type="button">
+    Click me
+  </button>
+);
+
+export default Button;
