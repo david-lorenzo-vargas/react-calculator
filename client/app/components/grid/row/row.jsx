506b4db9057f797c297f14d75b75e20fd023ca12
unknown <dlorenzo.05@gmail.com>
2020-10-07 17:02:00 +0200

    indentation fixed in row.jsx inside grid component

--- client/app/components/grid/row/row.jsx
+++ client/app/components/grid/row/row.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
-import styles from './row.scss'
-import classnames from 'classnames/bind'
+import React from 'react';
+import classnames from 'classnames/bind';
+import styles from './row.scss';
 
-const cx = classnames.bind(styles)
+const cx = classnames.bind(styles);
 
 const Row = (props) => {
-  const { direction, children } = props
+  const { direction, children } = props;
 
   return (
     <div
       className={cx('grid', {
         [`grid--${direction}`]: direction,
-      })}>
+      })}
+    >
       {children}
     </div>
-  )
-}
+  );
+};
 
 Row.defaultProps = {
   direction: 'row',
-}
+};
 
-export default Row
+export default Row;
