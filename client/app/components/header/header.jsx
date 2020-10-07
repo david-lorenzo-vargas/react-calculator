7e8777e99de2fe1342163b27364ad05a5a1ea36a
unknown <dlorenzo.05@gmail.com>
2020-10-07 17:10:41 +0200

    indentation fixed in header.jsx inside header component

--- client/app/components/header/header.jsx
+++ client/app/components/header/header.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
+import React from 'react';
 
-import styles from './header.scss'
+import styles from './header.scss';
 
-const Header = (props) => {
-  return (
-    <div className={styles.calculator__header}>
-      <span className={styles.calculator__header__total}>{props.total}</span>
-    </div>
-  )
-}
+const Header = (props) => (
+  <div className={styles.calculator__header}>
+    <span className={styles.calculator__header__total}>{props.total}</span>
+  </div>
+);
 
-export default Header
+export default Header;
