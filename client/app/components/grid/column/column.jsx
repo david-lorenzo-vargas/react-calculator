b01f277341bef8ab422aa8e877e5cd3320511188
unknown <dlorenzo.05@gmail.com>
2020-10-07 16:17:19 +0200

    indentation fixed in column.jsx

--- client/app/components/grid/column/column.jsx
+++ client/app/components/grid/column/column.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import classnames from 'classnames/bind'
+import React from 'react';
+import classnames from 'classnames/bind';
 
-import styles from './column.scss'
+import styles from './column.scss';
 
-const cx = classnames.bind(styles)
+const cx = classnames.bind(styles);
 
 const Column = (props) => {
-  const { children, grow, shrink } = props
+  const { children, grow, shrink } = props;
 
   const classes = cx('grid__item', {
     'grid__item--grow': grow,
     'grid__item--shrink': shrink,
-  })
+  });
 
-  return <div className={classes}>{children}</div>
-}
+  return <div className={classes}>{children}</div>;
+};
 
-export default Column
+export default Column;
