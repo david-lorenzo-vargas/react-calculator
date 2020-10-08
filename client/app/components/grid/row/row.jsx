0c7cc8d456c09a28c7802b4a0a16d1dae074cf72
unknown <dlorenzo.05@gmail.com>
2020-10-08 16:24:45 +0200

    lint fixed in row.jsx inside grid component

--- client/app/components/grid/row/row.jsx
+++ client/app/components/grid/row/row.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classnames from 'classnames/bind';
 import styles from './row.scss';
 
@@ -16,6 +17,11 @@ const Row = (props) => {
       {children}
     </div>
   );
+};
+
+Row.propTypes = {
+  children: PropTypes.node,
+  direction: PropTypes.bool,
 };
 
 Row.defaultProps = {
