17e5be2484375d7dba7fbea14f5cb575c4954767
unknown <dlorenzo.05@gmail.com>
2020-10-08 16:23:52 +0200

    lint fixed in header.jsx inside header component

--- client/app/components/header/header.jsx
+++ client/app/components/header/header.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './header.scss';
 
-const Header = (props) => (
-  <div className={styles.calculator__header}>
-    <span className={styles.calculator__header__total}>{props.total}</span>
-  </div>
-);
+const Header = (props) => {
+  const { total } = props;
+
+  return (
+    <div className={styles['calculator-header']}>
+      <span className={styles['calculator-header__total']}>{total}</span>
+    </div>
+  );
+};
+
+Header.propTypes = {
+  total: PropTypes.number,
+};
 
 export default Header;
